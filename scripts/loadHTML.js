c0d705c062e2a8554a861f71d018ef1f97611fbd
Nicole Jin <56412314+nicolejinn@users.noreply.github.com>
2022-08-09 18:25:09 +0000

    Changed approach in linking HTML files, fully functioning

---
+++ scripts/loadHTML.js
@@ -0,0 +1,22 @@
+loadFile("header", "htmls/header.html");
+loadFile("navbar", "htmls/nav.html");
+loadFile("daily-summary-section", "htmls/dailySummary.html");
+loadFile("log-section", "htmls/log.html");
+loadFile("footer", "htmls/about.html");
+
+function loadFile(elmntID, file) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file);
+
+  // load the page asynchronously
+    xhr.addEventListener('readystatechange', function() { 
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // if the file is correctly loaded
+
+            document.getElementById(elmntID).innerHTML = xhr.responseText; 
+        }
+    });
+
+    xhr.send(null); 
+
+}
