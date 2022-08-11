8c0b11483c122cc797f34455ad728411f6008e00
Nicole Jin <56412314+nicolejinn@users.noreply.github.com>
2022-08-11 03:12:35 +0000

    Fixed navbar animation + add email link to footer

--- scripts/navScript.js
+++ scripts/navScript.js
@@ -18,62 +18,67 @@ const dailySumPos = dailySum.offsetTop + dailySum.offsetHeight/2;
 const log = document.querySelector("#log-section");
 const logPos = (log.offsetTop + log.offsetHeight)/2;
 
+const dailySumOffset = 350;
+const windowOffset = 25;
+
+let ifSmoothScroll = false;
+
+navAnimation();
+
 // Navigate to corresponding location when button pressed
 dailySumButton.addEventListener('click', (e) => {
   selectDailySum();
+  ifSmoothScroll = true;
+  window.scrollTo(0, dailySumPos-dailySumOffset);
 });
 
 navLogButton.addEventListener('click', (e) => {
   selectLog();
+  ifSmoothScroll = true;
+  window.scrollTo(0, logPos);
 });
 
 aboutButton.addEventListener('click', (e) => {
   selectAbout();
+  ifSmoothScroll = true;
+  window.scrollTo(0, document.body.scrollHeight);
 });
-
-// When the user scrolls the page, execute navAnimation
-window.onscroll = function() { navAnimation() };
 
 function selectDailySum(){
   dailySumButton.classList.add("selected");
   navLogButton.classList.remove("selected");
   aboutButton.classList.remove("selected");
-  location.href ="#daily-summary";
 }
 
 function selectLog(){
   dailySumButton.classList.remove("selected");
   navLogButton.classList.add("selected");
   aboutButton.classList.remove("selected");
-  location.href ="#log";
 }
 
 function selectAbout(){
   dailySumButton.classList.remove("selected");
   navLogButton.classList.remove("selected");
   aboutButton.classList.add("selected");
-  location.href ="#footer";
 }
 
+// When the user scrolls the page, execute navAnimation
+window.onscroll = function() {navAnimation()};
+
 function navAnimation() {
-  if (window.pageYOffset >= sticky) {
+  if (window.scrollY >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
-
-  if (dailySumPos > window.pageYOffset) {//daily summary
-    dailySumButton.classList.add("selected");
-    navLogButton.classList.remove("selected");
-    aboutButton.classList.remove("selected");
-  } else if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) { // about us
-    dailySumButton.classList.remove("selected");
-    navLogButton.classList.remove("selected");
-    aboutButton.classList.add("selected");
-  } else{ // log
-    dailySumButton.classList.remove("selected");
-    navLogButton.classList.add("selected");
-    aboutButton.classList.remove("selected");
+  
+  if (!ifSmoothScroll){
+    if (dailySumPos > window.scrollY) {//daily summary
+      selectDailySum();
+    } else if ((window.innerHeight + window.scrollY + windowOffset) >= document.body.scrollHeight) { // about us
+      selectAbout();
+    } else{ // log
+      selectLog();
+    }
   }
 }
-// 
